996b27564336c0fd685d9d0f8dc209e746174076
Cedric Vidal <cedric@vidal.biz>
2024-03-13 17:30:46 +0000

    Catching intent parsing error

--- contoso-intent/parse_intent.py
+++ contoso-intent/parse_intent.py
@@ -4,5 +4,8 @@ import json
 
 @tool
 def extract_intent(input: str) -> str:
-    answer = json.loads(input)
+    try:
+        answer = json.loads(input)
+    except:
+        return 'error'
     return answer['intent']
